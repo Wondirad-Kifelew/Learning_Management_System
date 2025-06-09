ab5df4e48007bcc2130370b60d46c58f00b3f3af
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-06-09 07:44:58 +0300

    edited index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ app.use(cors())
 //routes  
 app.get('/', (req, res)=>res.send("API is working"))
 app.post('/clerk', express.json(), clerkWebHooks)
-
+app.post('/api/users', express.json(), (req, res)=>{
+    // const newUser={}
+    console.log("req body of post to /api/uses: ", req.body)
+})
 const port = process.env.PORT || 5000
 
 app.listen(port, ()=>console.log(`server is running on port ${port}`))
