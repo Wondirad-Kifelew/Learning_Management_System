883f93f748ad3da9661ddaf9d488cd766372c420
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-06-10 18:31:31 +0300

    whooks update

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-// import { clerkWebHooks } from './controllers/webhooks.js'
-import User from './models/user.js'
+import { clerkWebHooks } from './controllers/webhooks.js'
+
 
 const app = express()
 
@@ -14,26 +14,23 @@ app.use(cors())
 
 //routes  
 app.get('/', (req, res)=>res.send("API is working"))
-app.post('/clerk', express.raw({ type: '*/*' }), (req, res)=>{
-    console.log("reqest: ", req.body)
-})
-app.use(express.json())
+app.post('/clerk', express.json(), clerkWebHooks)
 
-app.post('/api/users', async (req, res)=>{
+// app.post('/api/users', async (req, res)=>{
     
-    const body = req.body
-    console.log("body", body)
-    const newUser={
-     _id: body.id,
-     name: body.first_name,
-     email: body.email_addresses[0].email_address,
-    imageUrl: body.image_Url
-}
-await User.create(newUser)
-res.status(201).json({ message: 'User created', user: newUser });
-    // console.log("req body id: ", req.body.data.id)
+//     const body = req.body
+//     console.log("body", body)
+//     const newUser={
+//      _id: body.id,
+//      name: body.first_name,
+//      email: body.email_addresses[0].email_address,
+//     imageUrl: body.image_Url
+// }
+// await User.create(newUser)
+// res.status(201).json({ message: 'User created', user: newUser });
+//     // console.log("req body id: ", req.body.data.id)
     
-})
+// })
 const port = process.env.PORT || 5000
 
 app.listen(port, ()=>console.log(`server is running on port ${port}`))
