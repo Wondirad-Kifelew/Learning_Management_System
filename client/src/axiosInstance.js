ade1b30bab62426ad191b23b97e1daf17b3c0a5e
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-06-21 00:06:21 +0300

    added deployed backend url in front end file

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // http://localhost:5000
-    baseURL:`http://localhost:5000`,
+    baseURL:`https://lms-backend-rrhg.onrender.com`,
     withCredentials:true
 })
 
