a653773bd5368a20ad631cd882bb2a0d302acd11
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-06-21 15:18:04 +0300

    fixed some styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 
 createRoot(document.getElementById('root')).render(
@@ -16,9 +15,6 @@ createRoot(document.getElementById('root')).render(
       newestOnTop={false}
       closeOnClick
       theme="light"
-      progressClassName={({ type }) =>
-    type === 'success' ? 'bg-blue-600' : undefined//else the default like red for error
-  }
     />
      <AppContextProvider>
         <App />
